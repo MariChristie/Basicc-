1652136a7a3295605289ecf6c3b657895a1c1d88
= <cavaleiromiau@gmail.com>
2025-11-11 23:00:53 -0300

    s

--- Aula09/Aula09/Program.cs
+++ Aula09/Aula09/Program.cs
@@ -7,5 +7,41 @@ public class Program
     public static void Main()
     {
         Console.WriteLine("=========Operadores lógicos");
+
+        bool isLogged = true;
+        bool hasAdminAcess = false;
+
+        Console.WriteLine("\nInformações do Usuário");
+        Console.WriteLine("Usuário logado: " + isLogged);
+        Console.WriteLine("Acesso ao painel administrador: " + hasAdminAcess);
+
+        Console.WriteLine("\nPermissões");
+
+        if (isLogged || hasAdminAcess)
+        {
+            Console.WriteLine("Acesso permitido ao sistema");
+        }
+        else
+        {
+            Console.WriteLine("Acesso negado ao sistema");
+        }
+
+        if (isLogged && hasAdminAcess)
+        {
+            Console.WriteLine("Acesso permitido ao painel administrador");
+        }
+        else
+        {
+            Console.WriteLine("Acesso negado ao painel administrador");
+        }
+
+        if (isLogged == false)
+        {    
+            Console.WriteLine("Usuário não está logado");
+        }
+        else        
+        {
+            Console.WriteLine("Usuário está logado");
+        }
     }
 }
---
+++ Aula10/Aula10/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Aula10;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("=====Calculadora simples===== \n");
+        Console.WriteLine("Digite o primeiro número");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Digite o segundo número");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Escolha uma operação:");
+        Console.WriteLine("1. Adição (+):");
+        Console.WriteLine("2. Subtração (-):");
+        Console.WriteLine("3. Multiplicação (*):");
+        Console.WriteLine("4. Divisão (/):");
+        Console.WriteLine("Digite o número da operação:");
+        int operation =  Convert.ToInt32(Console.ReadLine());
+    }
+}
